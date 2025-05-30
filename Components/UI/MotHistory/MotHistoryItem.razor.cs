755a0adf74736bbb27f96714b834ba4ab2dfbff0
Dylan Irwin <Dylan.Irwin@kerridgecs.com>
2025-05-30 16:43:47 +0100

    Added JS interop to pass screen size to Razor, allowing shrinking/growing of elements.

--- Components/UI/MotHistory/MotHistoryItem.razor.cs
+++ Components/UI/MotHistory/MotHistoryItem.razor.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
 using VehicleInformationChecker.Components.Models;
 using static VehicleInformationChecker.Components.Models.MotDefectModel;
 
 namespace VehicleInformationChecker.Components.UI.MotHistory;
 public partial class MotHistoryItem
 {
+    [Inject]
+    private IJSRuntime? JsRuntime { get; set; }
+
     [Parameter]
     public MotModel? Mot { get; set; }
 
@@ -19,4 +24,31 @@ public partial class MotHistoryItem
     private IEnumerable<MotDefectModel> _otherDefects =>
         Mot?.Defects.Where(d => !(d.Type == MotDefectType.Dangerous || d.Dangerous || d.Type == MotDefectType.Fail || d.Type == MotDefectType.Major))
         ?? Enumerable.Empty<MotDefectModel>();
+
+    private Size _iconSize = Size.Large;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender && JsRuntime != null)
+        {
+            var size = await JsRuntime.InvokeAsync<int>("getWindowWidth");
+
+            switch (size)
+            {
+                case < 600:
+                    _iconSize = Size.Small;
+                    break;
+                case < 960:
+                    _iconSize = Size.Medium;
+                    break;
+                default:
+                    _iconSize = Size.Large;
+                    break;
+            }
+
+            StateHasChanged();
+        }
+    }
 }
